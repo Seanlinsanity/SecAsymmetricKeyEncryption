41dca39c4f435d4fa4c5e15d0a3881aa723bfdc5
Sean Lin <seanlin1016@gmail.com>
2020-12-10 17:39:05 +0800

    Sign and verify message with SecKey

--- SecKeyEncryption/ViewController.swift
+++ SecKeyEncryption/ViewController.swift
@@ -9,10 +9,110 @@ import UIKit
 
 class ViewController: UIViewController {
     let message = "this is a sensitive message"
+    let signedMesssag = "this is a signed message"
+    let enclaveKeyTag = "mySecureEnclaveKeyTag"
+    let access = SecAccessControlCreateWithFlags(kCFAllocatorDefault,
+                                                 kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
+                                                 [.privateKeyUsage,.biometryAny],
+                                                 nil)!
+    lazy var secEnclaveTag = enclaveKeyTag.data(using: .utf8)! //1
+    lazy var privateKeyParams: [String: AnyObject] = [
+        kSecAttrIsPermanent as String: true as AnyObject,
+        kSecAttrApplicationTag as String: secEnclaveTag as AnyObject,
+        kSecAttrAccessControl as String: access
+    ]
+    lazy var attributes = [
+        kSecAttrKeyType as String: kSecAttrKeyTypeECSECPrimeRandom,
+        kSecAttrKeySizeInBits as String: 256,
+        kSecAttrTokenID as String: kSecAttrTokenIDSecureEnclave,
+        kSecPrivateKeyAttrs as String: privateKeyParams
+    ] as CFDictionary
+    
+    lazy var getQuery: [String: Any] = [kSecClass as String: kSecClassKey,
+                                   kSecAttrApplicationTag as String: enclaveKeyTag,
+                                   kSecReturnRef as String: true,
+                                   kSecAttrAccessControl as String: access
+                                ]
+    
     private var publicKeySec, privateKeySec: SecKey?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        executeSignMsg()
+        executeAsymmetricEncryption()
+    }
+    
+    private func executeSignMsg() {
+        guard let privateKey = createSignedKey(), let publicKey = SecKeyCopyPublicKey(privateKey) else {
+            print("Public key generation error")
+            return
+        }
+
+        guard let signedString = signMessage(privateKey: privateKey) else { return }
+        let isSuccess = verifySignedMessageString(publicKey: publicKey, signedString: signedString)
+        print("verify signed message: ", isSuccess)
+    }
+    
+    private func createSignedKey() -> SecKey?{
+        //Creating the Access Control Object
+        var item: CFTypeRef?
+        var key: SecKey?
+        let status = SecItemCopyMatching(getQuery as CFDictionary, &item)
+        if status == errSecSuccess {
+            key = (item as! SecKey)
+        } else if status == errSecItemNotFound {
+            var error: Unmanaged<CFError>?
+            guard let newKey = SecKeyCreateRandomKey(attributes as CFDictionary, &error) else {
+                print(error!.takeRetainedValue() as Error)
+                return nil
+            }
+            key = newKey
+        } else {
+            print("key query failed")
+        }
+        return key
+    }
+    
+    private func signMessage(privateKey: SecKey) -> String? {
+        guard let messageData = signedMesssag.data(using: String.Encoding.utf8) else {
+            print("Invalid message to sign.")
+            return nil
+        }
+        
+        var signError: Unmanaged<CFError>?
+        guard let signData = SecKeyCreateSignature(
+                privateKey,
+                SecKeyAlgorithm.ecdsaSignatureMessageX962SHA256,
+                messageData as CFData,
+                &signError) else {
+            print("Signing Error: ", signError ?? "empty error")
+            return nil
+        }
+        let signedData = signData as Data
+        let signedString = signedData.base64EncodedString(options: [])
+        print("Signed String", signedString)
+        return signedString
+    }
+    
+    private func verifySignedMessageString(publicKey:SecKey, signedString: String) -> Bool {
+        guard let messageData = signedMesssag.data(using: String.Encoding.utf8) else {
+            print("Invalid message to sign.")
+            return false
+        }
+        guard let signedMessageData = Data(base64Encoded: signedString) else {
+            print("Invalid message to verify.")
+            return false
+        }
+        let verify = SecKeyVerifySignature(
+            publicKey,
+            SecKeyAlgorithm.ecdsaSignatureMessageX962SHA256,
+            messageData as CFData,
+            signedMessageData as CFData,
+        nil)
+        return verify
+    }
+    
+    private func executeAsymmetricEncryption() {
         (publicKeySec, privateKeySec) = generateSecKeyPair()
         guard let publicKey = publicKeySec, let privateKey = privateKeySec else {
             print("invalid key pair")
@@ -85,6 +185,14 @@ class ViewController: UIViewController {
             return (publicKeySec, privateKeySec)
         }
     }
-
+    
+    private func deleteKey() {
+        let delStatus = SecItemDelete(getQuery as CFDictionary)
+        if delStatus == errSecSuccess {
+           print("deleted key")
+        } else {
+            print("There was a problem deleting the key")
+        }
+    }
 }
 
